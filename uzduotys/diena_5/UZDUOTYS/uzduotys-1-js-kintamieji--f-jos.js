a06ae6d9e712ac850d0132194b621ca25eff30b7
ahzel <lukasmartinaitis1995@gmail.com>
2019-07-24 09:51:02 +0300

    .

--- uzduotys/diena_5/UZDUOTYS/uzduotys-1-js-kintamieji--f-jos.js
+++ uzduotys/diena_5/UZDUOTYS/uzduotys-1-js-kintamieji--f-jos.js
@@ -27,11 +27,11 @@
 // sukurti funkcija "printTekstas(x)" ,
 //  kuri atspausdina i konsole "x" reiksmes
 // iskviesti f-ja ir vietoj x irasyti koki nors teksta pvz: printTekstas( "Jokubo istorijos");
-
+// printTekstas .js faile ir susivedi funkcijos tekse, o ne ivedi per console
 
 
 // 7 UZDUOTIS
-// F-ja kuri paskaiciuoja trikampio ilgaja krastine (Pitagoro teorema)
+// F-ja kuri paskaiciuoja staciojo trikampio ilgaja krastine (Pitagoro teorema)
 // pitagoroTeorema(x, y)      x*x + y*y ir is visko istraukti sakti (js Math.sqrt())
 
 //==========================KOMANDINE (3-4h) Bootstrap && GIT=================
