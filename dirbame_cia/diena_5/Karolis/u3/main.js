7a6fbb83240806a3e5be2ae6b75a2591cb4b3883
ahzel <lukasmartinaitis1995@gmail.com>
2019-07-24 16:39:58 +0300

    07-24

---
+++ dirbame_cia/diena_5/Karolis/u3/main.js
@@ -0,0 +1,5 @@
+console.log("labas");
+
+for ( var i = 0; i < 5 ; i++) {
+    console.log( i );
+}
---
+++ uzduotys/diena_5/UZDUOTYS/uzduotys-1-js-kintamieji--f-jos+++++2.js
@@ -0,0 +1,42 @@
+//  1 UZDUOTIS
+// sukurti kintamuosius (ir jiems priskirti reiksmes):
+// vardas, pavarde, amzius, atlyginimas
+// ir panaudoti:
+// console.log( vardas, pavarde);
+
+// teo:
+//  typeof( k ); f-ja kuri isveda kintamojo tipa
+// console.log(    typeof(vardas)  );
+// console.log(    typeof(amzius)  );
+
+//  2 UZDUOTIS
+// sukurti funkcija "printVardasPavardeAmzius()" , kuri atspausdina i konsole pirmos uzduoties kintamuosius
+
+
+//  3 UZDUOTIS
+// sukurti funkcija "printMetinisPajamuDydis()" ,
+// kuri  atspausdina i konsole suma 12 atlyginimu (vienas atlyginimas yra lygus "uzduotis 1" kintamajam - "atlyginimas")
+
+
+//  4 UZDUOTIS
+// A) sukurti kintamuosius:  salis, miestas, adresas, pastoKodas
+// B) sukurti f-ja "printAddressData()",  kuri atspausdina i konsole visus siuos kintamuosius
+
+// ---- advance-----
+//  5 UZDUOTIS
+// sukurti funkcija "printTekstas(x)" ,
+//  kuri atspausdina i konsole "x" reiksmes
+// iskviesti f-ja ir vietoj x irasyti koki nors teksta pvz: printTekstas( "Jokubo istorijos");
+// printTekstas .js faile ir susivedi funkcijos tekse, o ne ivedi per console
+
+
+// 7 UZDUOTIS
+<<<<<<< HEAD:uzduotys/diena_5/UZDUOTYS/uzduotys-1-js-kintamieji--f-jos.js
+// F-ja kuri paskaiciuoja staciojo trikampio ilgaja krastine (Pitagoro teorema)
+=======
+// F-ja kuri paskaiciuoja  staciojo trikampio ilgaja krastine (Pitagoro teorema)
+>>>>>>> ce36474d479998459ce3cc2aa5a8b1009064655d:uzduotys/diena_5/UZDUOTYS/uzduotys-1-js-kintamieji--f-jos+++++2.js
+// pitagoroTeorema(x, y)      x*x + y*y ir is visko istraukti sakti (js Math.sqrt())
+
+//==========================KOMANDINE (3-4h) Bootstrap && GIT=================
+// ant lentos
