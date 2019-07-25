bd50e5b6e46254ff6bba4d08ab514e1766475307
VaidaMi <40764397+VaidaMi@users.noreply.github.com>
2019-07-25 09:34:30 +0300

    sukurti nauji

---
+++ dirbame_cia/diena_5/Vaida/3-js-ciklai/main.js
@@ -0,0 +1,20 @@
+console.log("labas");
+
+
+let t = "#";
+for (var i = 0; i < 6; i++) {
+
+    console.log( t );
+    t = t + "#";
+    // t+= "#";
+
+}
+
+// 1 UZDUOTIS
+// naudojant FOR cikla, atspausdinti  "#" tiek, kad gautusi trikampio vaizdas
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
