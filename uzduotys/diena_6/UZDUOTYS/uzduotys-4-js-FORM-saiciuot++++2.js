a94965f7d89a99b6067179f64a7e522eb97c3891
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-07-25 09:26:59 +0300

    fixed day 6

--- uzduotys/diena_6/UZDUOTYS/uzduotys-4-js-FORM-saiciuot++++2.js
+++ uzduotys/diena_6/UZDUOTYS/uzduotys-4-js-FORM-saiciuot++++2.js
@@ -8,15 +8,15 @@
 
 //============== =========
 // UZDUOTIS 1
-sukurti ivedimo lauka, naudojantis pvz 3, 
+sukurti ivedimo lauka, naudojantis pvz 3,
 kur reikes ivesti apskritimo spinduli ,,R".
 Kai paspaudziamas mygtukas - paskaiciuojamas apskritimo plotas: Plotas = pi * R*R
-pi = Math.pi;
- 
+pi = Math.PI;
+
 
 
  // UZDUOTIS 2
-sukurti ivedimo 2 laukus, naudojantis pvz 3, 
+sukurti ivedimo 2 laukus, naudojantis pvz 3,
 kur reikes ivesti staciakampio auksti ir ploti.
-Kai paspaudziamas mygtukas - paskaiciuojamas staciakampio plotas:  
+Kai paspaudziamas mygtukas - paskaiciuojamas staciakampio plotas:
 Plotas  = x * y;
