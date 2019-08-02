da49200181eb23b8a32bd9112b8f2c5971b0cffd
vaidas1111 <vaidas.oberauskas@gmail.com>
2019-08-02 13:05:26 +0300

    0802 darbas

---
+++ dirbame_cia/diena_12/Vaidas/1-html-iskaidymas/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//grieztai reikalauja, jei jo nera - niekas neveikia
+// required_once("main.php");
+// required("main.php");
+//zodelis once, reiskia kad nauodosim viena karta.
+
+
+//nera pranesimo, kad yra du vienodi failai
+// include("header.php");
+// include("header.php");
+
+//turi buti pranesimas, kad yra du ienodi failai
+// include_once("header.php");
+
+
+
+include_once("header.php");
+
+include_once("main.php");
+
+include_once("footer.php");
+
+
+
+
+?>
