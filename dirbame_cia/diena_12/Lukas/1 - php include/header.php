12c26883c9f7279dd28e45c65bbb89dc20826212
livanauskas <ivanauskulukas@gmail.com>
2019-08-02 13:00:21 +0300

    uploading class work

---
+++ dirbame_cia/diena_12/Lukas/1 - php include/header.php
@@ -0,0 +1,10 @@
+<header class="bg-info aukstis-100">
+    <nav class="row">
+        <ul class="col-md-9">
+            <li>Home</li>
+            <li>About</li>
+            <li>Merchandise</li>
+            <li>Contacts</li>
+        </ul>
+    </nav>
+</header>
