d74a2e1e69d7cd524337d6060614e22186726aee
livanauskas <ivanauskulukas@gmail.com>
2019-08-14 17:00:44 +0300

    commit

---
+++ dirbame_cia/diena_19/Lukas/4 bs layout backup/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Layout No. 3</title>
+        <!-- reikalinga kad prisitaikantis dizainas veiktu tvarkingai -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+        <!-- galimos klaidos -->
+        <!-- blogas kelias iki failo -->
+        <!-- "/" ne i ta puse -->
+        <!-- neuzdarete ">" -->
+        <link rel="stylesheet" href="css/master.css">
+        <!-- !!!! VISSA MANO CSS failas pats zemiausias -->
+    </head>
+
+    <body>
+
+        <div class="container-fluid">
+            <header class="row bg-info aukstis-100 mr-2 ml-2">
+                <div class="col-md-12 pt-2">
+                    Header
+                </div>
+            </header>
+            <div class="row bg-info mt-2 mr-2 ml-2">
+                <p>mini juosta</p>
+
+            </div>
+            <!-- end mini juosta, header -->
+            <div class="row">
+                <div class="col-md-9 aukstis-1000 bg-info">
+                    <div class="row aukstis-300">
+
+                    </div>
+                    <div class="row aukstis-300">
+
+
+
+                    </div>
+                    <div class="row aukstis-300">
+
+                    </div>
+                </div>
+                
+                    <div class="row aukstis-200">
+
+                    </div>
+                    <div class="row aukstis-200">
+
+
+
+                    </div>
+                    <div class="row aukstis-200">
+
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+
+
+
+
+        <!-- bootstrap'ui -->
+        <script src="libs/jquery-3.4.1.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js">    </script>
+
+        <!-- mano js failas visasda pats zemiausias ir failo apacioje -->
+        <script type="text/javascript" src="main.js">
+
+        </script>
+
+
+    </body>
+</html>
