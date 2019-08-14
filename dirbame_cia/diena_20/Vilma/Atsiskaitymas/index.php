f521f96688139e220339bfa3f160617d1eae59bf
Villma-Dz <52885537+Villma-Dz@users.noreply.github.com>
2019-08-14 10:37:59 +0300

    20dienos atsiskaitymas

---
+++ dirbame_cia/diena_20/Vilma/Atsiskaitymas/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- reikalinga kad prisitaikantis dizainas veiktu tvarkingai -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+    <!-- galimos klaidos -->
+    <!-- blogas kelias iki failo -->
+    <!-- "/" ne i ta puse -->
+    <!-- neuzdarete ">" -->
+    <link rel="stylesheet" href="css/master.css">
+    <!-- !!!! VISSA MANO CSS failas pats zemiausias -->
+</head>
+
+<body>
+    <div class="container-fluid">
+        <header class="bg-info aukstis-100 ml-1 mr-1 mt-3">
+            <p>Top header iliustration</p>
+        </header>
+        <div class="row bg-success mt-3 mr-1 mb-3 ml-1">
+            <p>Top meniu links</p>
+        </div>
+        <div class="row">
+            <div class="col-9 ml-3 mr-3">
+                <div class="row bg-warning aukstis-300">
+                    <p>Latest Blog Post 1</p>
+                </div>
+                <div class="row bg-info aukstis-300 mt-3 mb-3">
+                    <p>Latest Blog Post 2</p>
+                </div>
+                <div class="row bg-warning  aukstis-330">
+                    <p>Latest Blog Post 3</p>
+                </div>
+            </div>
+            <div class="col mr-3">
+                <div class="row bg-info aukstis-200">
+                    <p>newsletter 1</p>
+                </div>
+                <div class="row bg-warning  aukstis-200 mt-3 mb-3">
+                    <p>Read</p>
+                </div>
+                <div class="row bg-info aukstis-250">
+                    <p>Creditable</p>
+                </div>
+                <div class="row   aukstis-150 mt-3 mb-3">
+                    <div class="col bg-warning mr-3">
+                        <p>Baner</p>
+                    </div>
+                    <div class="col bg-success">
+                        <p>Or this</p>
+                    </div>
+                </div>
+                <div class="row bg-info aukstis-100">
+                    <p>Social media</p>
+                </div>
+            </div>
+        </div>
+
+        <footer class="bg-warning mt-3">
+            <p>Footer area</p>
+        </footer>
+
+    </div>
+
+
+
+    <!-- bootstrap'ui -->
+    <script src="libs/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- mano js failas visasda pats zemiausias ir failo apacioje -->
+    <script type="text/javascript" src="main.js">
+
+    </script>
+
+
+</body>
+
+</html>
