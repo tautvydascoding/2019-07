4cfb25c7b44d58b582615a6d39b35e32881fa725
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-08-14 17:15:01 +0300

    klases darbas

---
+++ dirbame_cia/diena_20/destytojas/4-php-email-siuntimas/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- reikalinga kad prisitaikantis dizainas veiktu tvarkingai -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+        <!-- galimos klaidos -->
+        <!-- blogas kelias iki failo -->
+        <!-- "/" ne i ta puse -->
+        <!-- neuzdarete ">" -->
+        <link rel="stylesheet" href="css/master.css">
+        <!-- !!!! VISSA MANO CSS failas pats zemiausias -->
+    </head>
+
+    <body>
+
+        <h1> Isijunk inspect->console </h1>
+
+        <!-- vardas -->
+        <!-- email -->
+        <!-- klausimas -->
+        <form  action="siustiEmail.php" method="post">
+            <input type="text" name="vardas" value="" placeholder="jusu vardas">
+            <input type="text" name="pastas" value=""  placeholder="jusu el. pastas">
+            <textarea name="klausimas" rows="8" cols="80">      </textarea>
+
+            <button type="submit"  >  KLAUSTI </button>
+        </form>
+
+
+        <!-- bootstrap'ui -->
+        <script src="libs/jquery-3.4.1.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js">    </script>
+
+        <!-- mano js failas visasda pats zemiausias ir failo apacioje -->
+        <script type="text/javascript" src="main.js">
+
+        </script>
+
+
+    </body>
+</html>
