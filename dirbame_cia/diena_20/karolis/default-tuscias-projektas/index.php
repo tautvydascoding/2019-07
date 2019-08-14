868f1e6a25b53899bbab32ab29be6c887de039f8
ahzel <lukasmartinaitis1995@gmail.com>
2019-08-14 16:52:56 +0300

    trec-14-pab

---
+++ dirbame_cia/diena_20/karolis/default-tuscias-projektas/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- reikalinga kad prisitaikantis dizainas veiktu tvarkingai -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+        <!-- galimos klaidos -->
+        <!-- blogas kelias iki failo -->
+        <!-- "/" ne i ta puse -->
+        <!-- neuzdarete ">" -->
+        <link rel="stylesheet" href="css/master.css">
+        <!-- !!!! VISSA MANO CSS failas pats zemiausias -->
+    </head>
+
+    <body>
+
+        <div class="container">
+
+                <div class="block1  r aukstis-100">
+
+                </div>
+                <div class="block2  b aukstis-100">
+
+                </div>
+                <div class="block3  r aukstis-100">
+
+                </div>
+                <div class="clearfix"></div>
+                <div class="block4 g aukstis-200">
+
+                </div>
+
+        </div>
+
+
+
+
+        <!-- bootstrap'ui -->
+        <script src="libs/jquery-3.4.1.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- mano js failas visasda pats zemiausias ir failo apacioje -->
+        <script type="text/javascript" src="main.js">
+
+        </script>
+
+
+    </body>
+</html>
